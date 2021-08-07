f6d78b36cb0823e694829660c1e1cc41d7eb0258
Vilian P <vilkata41@gmail.com>
2021-08-07 18:58:00 +0300

    Setting up Python project

---
+++ main.py
@@ -0,0 +1,9 @@
+import mediapipe as mp
+import cv2
+
+mp_face_detection = mp.solutions.face_detection
+mp_drawing = mp.solutions.drawing_utils
+
+if __name__ == '__main__':
+    print('hi' + 'PyCharm')
+
